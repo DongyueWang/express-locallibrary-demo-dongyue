3a719c8b5c551f70fde92d748ced013ed875add8
Dongyue <dongyue.wang@external.loccitane.com>
2021-11-13 12:10:56 +0100

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://dongyue:wsyb1452@cluster0.klkxq.mongodb.net/local_library?retryWrites=true&w=majority';
+
+//var mongoDB = 'mongodb+srv://dongyue:wsyb1452@cluster0.klkxq.mongodb.net/local_library?retryWrites=true&w=majority';
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://dongyue:wsyb1452@cluster0.klkxq.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
